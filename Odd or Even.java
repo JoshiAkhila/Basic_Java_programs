5b46ef3336ca069a0a25684fe45bae04c5b9c636
JoshiAkhila <joshi.akhila@gmail.com>
2017-11-07 21:05:59 -0800

    find whetherthe number is odd or even

---
+++ Odd or Even.java
@@ -0,0 +1,17 @@
+public class OddandEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enetr the number");
+		int n = in.nextInt();
+		
+		if(n%2!=0){
+			System.out.println("entered number is  odd");
+		}
+		else 
+			System.out.println("entered number is  even");
+	}
+
+}
